38d0b31cc2fdb9418ec0db0b4ef2a4f80194dac6
ray821225 <h24172037@gmail.com>
2021-04-06 21:11:03 +0800

    add products/services/signup components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 const App = () => {
   return (
@@ -9,7 +12,10 @@ const App = () => {
       <BrowserRouter>
         <Navbar></Navbar>
         <Switch>
-          <Route path="/" exact component={Home}></Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -24,15 +24,21 @@ const Cards = () => {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-9.jpg"
-              text="Explore the hidden waterfall deep inside the Amzone Jungle"
+              src="images/img-3.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="images/img-2.jpg"
+              src="images/img-8.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
+              label="Adrenaline"
               path="/services"
             />
           </ul>
